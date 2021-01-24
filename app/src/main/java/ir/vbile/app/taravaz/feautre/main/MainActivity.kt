3bc94c0f991508a64a013e7fe471fb456c84c33d
Vahid Garousi <garousiv@gmail.com>
2021-01-24 08:16:21 -0500

    vahid: dependencies added Base.kt added. and some structure added.

---
+++ app/src/main/java/ir/vbile/app/taravaz/feautre/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.vbile.app.taravaz.feautre.main
+
+import android.os.Bundle
+import ir.vbile.app.taravaz.R
+import ir.vbile.app.taravaz.common.TarAvazActivity
+
+class MainActivity : TarAvazActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
