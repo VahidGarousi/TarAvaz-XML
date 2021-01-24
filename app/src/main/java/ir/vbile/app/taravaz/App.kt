bf9973020d4a942432116793ddac67f3c7081e7b
Vahid Garousi <garousiv@gmail.com>
2021-01-24 09:08:07 -0500

    vahid: navigation resource added.

--- app/src/main/java/ir/vbile/app/taravaz/App.kt
+++ app/src/main/java/ir/vbile/app/taravaz/App.kt
@@ -1,9 +1,12 @@
 package ir.vbile.app.taravaz
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 
 class App : Application(){
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
     }
 }
