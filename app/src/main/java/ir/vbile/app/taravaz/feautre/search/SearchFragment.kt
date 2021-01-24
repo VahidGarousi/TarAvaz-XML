bf82be207bb896e0652e32e5fdf6071c10705450
Vahid Garousi <garousiv@gmail.com>
2021-01-24 09:10:42 -0500

    vahid: SearchFragment.kt added.

---
+++ app/src/main/java/ir/vbile/app/taravaz/feautre/search/SearchFragment.kt
@@ -0,0 +1,6 @@
+package ir.vbile.app.taravaz.feautre.search
+
+import ir.vbile.app.taravaz.R
+import ir.vbile.app.taravaz.common.TarAvazFragment
+
+class SearchFragment : TarAvazFragment(R.layout.fragment_home)
