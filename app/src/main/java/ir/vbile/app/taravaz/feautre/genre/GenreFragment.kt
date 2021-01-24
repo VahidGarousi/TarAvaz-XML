fecfaddfac1203cc175717a8fe1028c496f24d7d
Vahid Garousi <garousiv@gmail.com>
2021-01-24 09:09:42 -0500

    vahid: GenreFragment.kt added.

---
+++ app/src/main/java/ir/vbile/app/taravaz/feautre/genre/GenreFragment.kt
@@ -0,0 +1,6 @@
+package ir.vbile.app.taravaz.feautre.genre
+
+import ir.vbile.app.taravaz.R
+import ir.vbile.app.taravaz.common.TarAvazFragment
+
+class GenreFragment : TarAvazFragment(R.layout.fragment_genre)
