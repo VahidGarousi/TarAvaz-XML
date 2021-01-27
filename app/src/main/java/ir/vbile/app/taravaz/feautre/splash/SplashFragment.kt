6ab16e0cd271155c6a950a7c2ecb3cfa1cd0de28
Vahid Garousi <garousiv@gmail.com>
2021-01-27 09:46:01 +0330

    vahid: SplashActivity.kt deleted and SplashFragment.kt added.

---
+++ app/src/main/java/ir/vbile/app/taravaz/feautre/splash/SplashFragment.kt
@@ -0,0 +1,8 @@
+package ir.vbile.app.taravaz.feautre.splash
+
+import ir.vbile.app.taravaz.R
+import ir.vbile.app.taravaz.common.TarAvazFragment
+
+class SplashFragment : TarAvazFragment(R.layout.fragment_splash) {
+
+}
