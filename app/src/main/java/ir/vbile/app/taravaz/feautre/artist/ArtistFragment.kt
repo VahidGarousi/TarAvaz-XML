eadd069aeb3a87470e7669c6507e371941d0fef7
Vahid Garousi <garousiv@gmail.com>
2021-02-06 12:28:05 +0330

    vahid: fragment_artist.xml finished.

--- app/src/main/java/ir/vbile/app/taravaz/feautre/artist/ArtistFragment.kt
+++ app/src/main/java/ir/vbile/app/taravaz/feautre/artist/ArtistFragment.kt
@@ -1,6 +1,19 @@
 package ir.vbile.app.taravaz.feautre.artist
 
+import android.os.Bundle
+import android.view.View
 import ir.vbile.app.taravaz.R
 import ir.vbile.app.taravaz.common.TarAvazFragment
+import kotlinx.android.synthetic.main.fragment_artist.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class ArtistFragment : TarAvazFragment(R.layout.fragment_artist)
+class ArtistFragment : TarAvazFragment(R.layout.fragment_artist) {
+    val vm: ArtistVM by viewModel()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm.artists.observe(viewLifecycleOwner) {
+            rowSuggestedArtists.submitList(it)
+            rowArtists.submitList(it)
+        }
+    }
+}
